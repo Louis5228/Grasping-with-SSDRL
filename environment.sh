d3ddfa23f065c37818cfc7d2d9da7bdfe011401f
kuolunwang <kuolunwang.ee08@nycu.edu.tw>
2021-06-27 12:02:23 +0800

    fix environment.sh

--- environment.sh
+++ environment.sh
@@ -2,8 +2,23 @@
 
 HOME=/home/dlp
 
-export ROS_IP=127.0.0.1
-export ROS_MASTER_URI=http://127.0.0.1:11311
+export ROS_MASTER_URI=http://140.113.148.82:11311
+
+if [ "$1" ]; then
+    echo "ROS IP $1"
+    export ROS_IP=$1
+else
+    echo "ROS IP 127.0.0.1"
+    export ROS_IP=127.0.0.1
+fi
+
+if [ "$2" ]; then
+    echo "ROS MASRER $2"
+    export ROS_MASTER_URI=http://$2:11311
+else
+    echo "ROS MASRER 127.0.0.1"
+    export ROS_MASTER_URI=http://127.0.0.1:11311
+fi
 
 source /opt/ros/melodic/setup.bash
 source ./catkin_ws/devel/setup.bash
